24826bdab97d73edab9ab65570f4c5540e8149be
trgyd <trgydmr17@gmail.com>
2023-01-30 21:45:59 +0300

    bitirelim şu işi

--- src/test1.java
+++ src/test1.java
@@ -2,6 +2,9 @@ public class test1 {
 
     public static void main(String[] args) {
         System.out.println("merhaba Grup 3");
+        System.out.println("hadi lead bitirelim şu işi  :)))))");
+        System.out.println("yollayalım hocaya :)))");
+
 
     }
 
